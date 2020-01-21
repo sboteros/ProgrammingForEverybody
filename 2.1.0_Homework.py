4640ab284b57bc6c63f805435cd4c281ff288e2e
Santiago Botero Sierra <sboteros@unal.edu.co>
2020-01-20 22:36:22 -0600

    Week 4 completed!

---
+++ 2.1.0_Homework.py
@@ -0,0 +1,9 @@
+# Week 2 - Programm that writes something
+# Programming for Everybody Course/
+# Python for Everybody Specialization
+# Santiago Botero Sierra
+# sboteros@unal.edu.co
+# 19/01/2020
+# Encoding: UTF-8
+
+print('Hi, my name is Santiago')
---
+++ 4.3_Assignment2.3.py
@@ -0,0 +1,12 @@
+# Week 4 - Programm that writes something
+# Programming for Everybody Course
+# Python for Everybody Specialization
+# Santiago Botero Sierra
+# sboteros@unal.edu.co
+# 20/01/2020
+# Encoding: UTF-8
+
+# 3. Program to compute gross pay
+hours = input('Enter hours:')
+rate = input('Enter rate:')
+print('Pay:', float(hours) * float(rate))
