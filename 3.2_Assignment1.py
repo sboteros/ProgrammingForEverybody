10df7e2d1c87377ec10d40a1e88112890eb79b0b
Santiago Botero Sierra <sboteros@unal.edu.co>
2020-01-23 19:11:53 -0600

    .gitignore fix - Assignments

--- 3.2_Assignment1.py
+++
@@ -1,9 +0,0 @@
-# Week 2 - Programm that writes something
-# Programming for Everybody Course
-# Python for Everybody Specialization
-# Santiago Botero Sierra
-# sboteros@unal.edu.co
-# 19/01/2020
-# Encoding: UTF-8
-
-print('hello world')
